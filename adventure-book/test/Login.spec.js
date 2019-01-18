6ea652be4522fa93d612b8c44f11fb16b6198545
Juan C. Glez. Pascolo <jcpascolo@gmail.com>
2019-01-18 10:04:30 +0000

    siguiendo con el teting

--- adventure-book/test/Login.spec.js
+++ adventure-book/test/Login.spec.js
@@ -5,6 +5,7 @@ describe("Login", () => {
     it('testing login', () => {
         const wrapper = shallowMount(Login)
         const msg = wrapper.find('h2').text();
+        debugger;
         expect(msg).toBe('Iniciar Sesión');
     })
 })

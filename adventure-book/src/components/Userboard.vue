cf29b360eaf62e32f413ae4211eaa3336184c8f6
Mireia Scholz <alu0100905144@ull.edu.es>
2019-01-31 23:16:56 +0000

    El botón del userboard es todo el ancho de la fila

--- adventure-book/src/components/Userboard.vue
+++ adventure-book/src/components/Userboard.vue
@@ -131,9 +131,9 @@
             <b-row>
                 <h2>{{ sitios_visitados[0].nombre }}</h2>
             </b-row>
-            <b-row id="icon_desc">
-                <img @click="upload()" width="18px" height="18px" src="../upload.png" alt="cerrar">
-                <p @click="upload()" > Subir una foto </p>
+            <b-row id="icon_desc" @click="upload()">
+                <img  width="18px" height="18px" src="../upload.png" alt="cerrar">
+                <p> Subir una foto </p>
             </b-row>
 
             <b-row v-if="sitios_visitados[0].sitios.length == 0">
